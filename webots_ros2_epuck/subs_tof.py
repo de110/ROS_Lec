f7b2b4d17287cd7889a83a299a54b9ca37541621
de110 <skyyks98@gmail.com>
2023-09-06 15:10:49 +0900

    feat: get obstacle distance

--- webots_ros2_epuck/subs_tof.py
+++ webots_ros2_epuck/subs_tof.py
@@ -4,11 +4,12 @@ from rclpy.node import Node
 from sensor_msgs.msg import Range
 from geometry_msgs.msg import Twist
 
-class subs_tof(Node):
+class subs_tof(Node): # Node 클래스
     
     def __init__(self):
-        super().__init__('subscriber_tof')
-        self.subscription = self.create_subscription(Range,'tof',self.listener_callback,10)
+        super().__init__('subscriber_tof') # ‘subscriber_tof’ 이름의 클래스 생성자 호출
+        self.subscription=self.create_subscription(Range,'tof',self.listener_callback,10)
+# subscriber 설정, (토픽 메시지 타입, 토픽 이름, 호출 함수, QoS)
         self.range=0
 
     def listener_callback(self,msg):
@@ -16,9 +17,9 @@ class subs_tof(Node):
         self.get_logger().info('sensor is seeing obstacle in distance: %f'%self.range)
 
 def main(args=None):
-    rclpy.init(args=args)
+    rclpy.init(args=args) # 초기화
 
-    minimal_subscriber = subs_tof()
+    minimal_subscriber = subs_tof() # subs_tof 클래스 생성
 
     rclpy.spin(minimal_subscriber)
 
@@ -26,5 +27,5 @@ def main(args=None):
     rclpy.shutdown()
 
 
-if __name__ == '__main__':
+if __name__=='__main__':
     main()    
