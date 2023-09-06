868f2e39e0cd86d42573ccfca9b9365ff82623f8
de110 <skyyks98@gmail.com>
2023-09-06 13:44:56 +0900

    ros

---
+++ src/test_launch/launch/demo.launch.py
@@ -0,0 +1,21 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    talker_node = Node(
+        package = "test_package_lde",
+        executable = "simple_publisher_node",
+    )
+
+    service_node = Node(
+        package = "test_package_lde",
+        executable = "service_server_node",
+    )
+
+    ld.add_action(talker_node)
+    ld.add_action(service_node)
+
+    return ld
+    
